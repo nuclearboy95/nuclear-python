79a498cf2b6c54ad045f0bdc4cad055a5d11ee96
jayjihun <t080205@gmail.com>
2019-07-06 07:23:14 +0900

    ver 0.10.16 modified ntf.Model.save

--- ntf/models/model.py
+++ ntf/models/model.py
@@ -17,32 +17,32 @@ class Model:
         vs = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=self.name)
         return tf.train.Saver(var_list=vs)
 
-    def save(self, sess, ckpt_path=None, name=None, verbose=True):
-        if name is None:
-            name = self.name
+    def save(self, sess, ckpt_path=None, save_name=None, verbose=True):
+        if save_name is None:
+            save_name = self.name
 
         if ckpt_path is None:
-            ckpt_path = './ckpts/%s/%s' % (name, name)
+            ckpt_path = './ckpts/%s/%s' % (save_name, save_name)
 
         os.makedirs(os.path.dirname(ckpt_path), exist_ok=True)
         self.saver.save(sess, ckpt_path)
 
         if verbose:
-            sayd("Model [{name}] saved to '{ckpt_path}'.".format(name=name, ckpt_path=ckpt_path))
+            sayd("Model [{name}] saved to '{ckpt_path}'.".format(name=self.name, ckpt_path=ckpt_path))
 
-    def load(self, sess, ckpt_path=None, name=None, ignore=False, verbose=True):
-        if name is None:
-            name = self.name
+    def load(self, sess, ckpt_path=None, save_name=None, ignore=False, verbose=True):
+        if save_name is None:
+            save_name = self.name
 
         if ckpt_path is None:
-            ckpt_path = './ckpts/%s/%s' % (name, name)
+            ckpt_path = './ckpts/%s/%s' % (save_name, save_name)
 
         try:
             self.saver.restore(sess, ckpt_path)
 
             if verbose:
-                sayd("Model [{name}] loaded from '{ckpt_path}'.".format(name=name, ckpt_path=ckpt_path))
+                sayd("Model [{name}] loaded from '{ckpt_path}'.".format(name=self.name, ckpt_path=ckpt_path))
         except:
-            sayw("Failed loading model [{name}] from '{ckpt_path}'.".format(name=name, ckpt_path=ckpt_path))
+            sayw("Failed loading model [{name}] from '{ckpt_path}'.".format(name=self.name, ckpt_path=ckpt_path))
             if not ignore:
                 raise
