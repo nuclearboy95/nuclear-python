60daa2f55d656bd10f60ce2e6b43139af5b7d841
jayjihun <t080205@gmail.com>
2019-08-20 12:04:13 +0900

    ver 0.10.35 added repeat

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('requirements.txt', 'r') as f:
 
 setup(name='nuclear-python',
 
-      version='0.10.34',
+      version='0.10.35',
 
       url='https://github.com/nuclearboy95/nuclear-python',
 
