81a0bd8b74559e785b4b809fe20a029bc2d67bd5
jayjihun <t080205@gmail.com>
2020-01-29 11:12:05 +0900

    ver 0.12.8.1 miscs

--- ntf/tb/read.py
+++ ntf/tb/read.py
@@ -9,9 +9,11 @@ __all__ = ['scalars']
 def scalars(fpath: str) -> list_of_dict:
     result = list()
     for event in tf.train.summary_iterator(fpath):
+        if len(event.summary.value) == 0:
+            continue
         d = dict()
         for v in event.summary.value:
-            d[v.tag] = d[v.simple_value]
+            d[v.tag] = v.simple_value
         d['step'] = event.step
 
         result.append(d)
