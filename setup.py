c7a9147dbe00b8e1f44cd75266fbad584d1c5741
jihun <t080205@gmail.com>
2021-01-07 12:31:46 +0900

    ver 0.15.5.2
    miscs.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('requirements.txt', 'r') as f:
 
 setup(name='nuclear-python',
 
-      version='0.15.5.1',
+      version='0.15.5.2',
 
       url='https://github.com/nuclearboy95/nuclear-python',
 
