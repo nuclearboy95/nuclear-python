1af1f445e6b80a9a1ea9487f3c67fe3bfd9e1eb0
jihun <t080205@gmail.com>
2021-03-29 17:23:12 +0900

    ver 0.15.7.6
    miscs

--- npy/log/setters.py
+++ npy/log/setters.py
@@ -8,9 +8,10 @@ _logger = None
 _sh = None
 LOGGING_TO_FILE = False
 LOGGING_TO_TELEGRAM = False
+DATE_FMT = "%d %b %H:%M:%S"
+LOG_FMT = '[%(asctime)s] %(message)s'
 
-
-__all__ = ['verbosity', 'save', 'telegram']
+__all__ = ['verbosity', 'save', 'telegram', 'set_datefmt', 'set_fmt']
 
 
 def _init():
@@ -22,22 +23,32 @@ def _init():
 
     _logger.setLevel(logging.DEBUG)
     level_styles = {
-                    'critical': {'color': 'red', 'bold': True},
-                    'error': {'color': 'yellow'},
-                    'warning': {'color': 'green', 'bold': True},
-                    'info': {},
-                    'debug': {'color': 'black', 'bright': True},
-                    }
+        'critical': {'color': 'red', 'bold': True},
+        'error': {'color': 'yellow'},
+        'warning': {'color': 'green', 'bold': True},
+        'info': {},
+        'debug': {'color': 'black', 'bright': True},
+    }
     field_styles = {
         'asctime': {'color': 'blue'},
         'levelname': {'color': 'yellow', 'faint': True}
     }
     coloredlogs.install(level='DEBUG', logger=_logger,
-                        fmt='[%(asctime)s] %(message)s',
-                        datefmt="%d %b %H:%M:%S",
+                        fmt=LOG_FMT,
+                        datefmt=DATE_FMT,
                         field_styles=field_styles,
                         level_styles=level_styles)
     _sh = _logger.handlers[0]
+
+
+def set_datefmt(v):
+    global DATE_FMT
+    DATE_FMT = v
+
+
+def set_fmt(v):
+    global LOG_FMT
+    LOG_FMT = v
 
 
 def save(fpath='log/log.log'):
@@ -92,7 +103,6 @@ def telegram(level=3):
     from .telegram_handler import TelegramHandler
     global LOGGING_TO_TELEGRAM
     if not LOGGING_TO_TELEGRAM:
-
         fmt = logging.Formatter('%(message)s')
         th = TelegramHandler()
         th.setFormatter(fmt)
