482516a19d738655395013117b25c14fda9bcf76
jihun <t080205@gmail.com>
2022-09-15 14:47:25 +0900

    ver 0.15.8
    Added video load / save

--- npy/files/io.py
+++ npy/files/io.py
@@ -3,6 +3,8 @@ import json
 import yaml
 import os
 import functools
+import cv2
+import numpy as np
 
 from .path import home_rsc_path, makedirpath
 
@@ -12,6 +14,7 @@ def ensure_dir_exist(f):
     def wrapper(content, fpath, *args, **kwargs):
         makedirpath(fpath)
         return f(content, fpath, *args, **kwargs)
+
     return wrapper
 
 
@@ -33,6 +36,37 @@ def load_yaml(fpath):
 def load_txt(fpath):
     with open(fpath, 'r') as f:
         return f.read()
+
+
+def load_video(fpath):
+    vidcap = cv2.VideoCapture(fpath)
+
+    ret = []
+    success = True
+    while success:
+        success, image = vidcap.read()
+        if success:
+            ret.append(image)
+
+    ret = np.stack(ret)
+    if ret.shape[-1] == 3:
+        ret = ret[..., [2, 1, 0]]
+    return ret
+
+
+@ensure_dir_exist
+def save_video(arr, fpath, fourcc='DIVX', fps=24, **kwargs):
+    fourcc = cv2.VideoWriter_fourcc(*fourcc)
+    N, H, W = arr.shape[:3]
+
+    if arr.shape[-1] == 3:  # BGR to RGB
+        arr = arr[..., [2, 1, 0]]
+
+    vid = cv2.VideoWriter(fpath, fourcc, fps, (W, H), **kwargs)
+
+    for frame in arr:
+        vid.write(frame)
+    vid.release()
 
 
 @ensure_dir_exist
